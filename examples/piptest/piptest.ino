f7716b91bbe1ee13977ab3e90f3f09b09a62b533
wwatson4506 <wwatson4506@users.noreply.github.com>
2019-11-30 16:46:56 -0800

    Update piptest.ino

--- examples/piptest/piptest.ino
+++ examples/piptest/piptest.ino
@@ -1,4 +1,14 @@
 // piptest.ino
+// This example demonstrates some of the different capabilities
+// of the RA8876. Formost, the two available PIP windows. PIP
+// window 1 will always be on top of PIP window 2. It uses the
+// setTMargins() function to create a window with a border that
+// has adjustable thickness and cycle the colors of that border.
+// It will repeatedly print the internal character set scrolling
+// the window up as it prints. The window is basically a terminal
+// window processing most ASCII control codes.
+// There is a built in status line at the bottom of the screen that
+// is 16 pixels in height with functions to support it.
 #include "Arduino.h"
 #include "Ra8876_Lite.h"
 #include "tft.h"
@@ -32,26 +42,21 @@ PROGMEM uint16_t myColors[] = {
 
 uint8_t onOff = 1; // Enable PIP
 uint8_t pipNo = 1; // PIP number 1
-
 uint16_t xp = 0;   // x coord. (source), for PIP
 uint16_t yp = 0;   // y coord. (source), for PIP
-
-uint16_t xd = 0;  // x coord. of displayed PIP window
+uint16_t xd = 0;   // x coord. of displayed PIP window
 uint16_t yd = 40;  // y coord. of displayed PIP window
 uint16_t xw = 320; // x width of displayed window
-uint16_t yh = 240; //y height of displayed window
-
+uint16_t yh = 240; // y height of displayed window
 const PROGMEM char *title ="PIP+ DEMO\n";
 const PROGMEM char *pip1 ="PIP WINDOW #1";
 const PROGMEM char *pip2 ="PIP WINDOW #2";
-
 uint8_t len = 0;
 uint8_t c = 32;
 
 int pipDelay = 400; // delayMicroseconds(pipDelay) Set to 0 for as fast as it gets
-				  // or tone it down?
+		    // or tone it down?
 uint16_t thickness = 10; // drawRect() line thickness (Brush Size)
-
 uint16_t x = 0;
 uint16_t y = 0;
 uint16_t w = 0;
@@ -83,7 +88,7 @@ void setup() {
 	tft_init();
 	initVT100();
 	setFontSize(2,false);
-    setTextAt(0,0);
+	setTextAt(0,0);
 	tft_cls(myColors[11]);
 	len = strlen(title);
 	setTextColorFG(myColors[6],myColors[11]);
@@ -95,28 +100,27 @@ void setup() {
 	
 	// Setup PIP window #1 on screen page #4
 	setTextColorFG(myColors[1],myColors[11]);
-	selectScreen(SCREEN_4);
+	selectScreen(SCREEN_4); // Select screen page 4
 	tft_cls(myColors[13]);
 	setFontSize(1,true); // Set 12x24 font size
-    setTextAt(0,0);
+	setTextAt(0,0);
 	setTextColorFG(myColors[0],myColors[13]);
 	tftPrintStr(pip1);
 	fillCircle(xw / 2, yh / 2, 100, myColors[2]);
 
 	// Setup PIP window #2 on screen page #5
 	setTextColorFG(myColors[1],myColors[11]);
-	selectScreen(SCREEN_5);
+	selectScreen(SCREEN_5); // Select screen page 5
 	tft_cls(myColors[6]);
 	setFontSize(2,true); // Set 16x32 font size
-    setTextAt(0,0);
+	setTextAt(0,0);
 	setTextColorFG(myColors[0],myColors[6]);
 	tftPrintStr(pip2);
 	fillRect(xw - 300, yh - 200 , 300, 200, myColors[15]);
 	// Set screen page #1 as the active screen page
-	selectScreen(SCREEN_1);
+	selectScreen(SCREEN_1); // Select home page
 }
 
-// DO IT
 void loop() {
 	char STR1[256];
 	uint32_t start = millis();
