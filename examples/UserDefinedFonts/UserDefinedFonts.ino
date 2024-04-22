8f68c4f511038e31354d22f3dbf108c798bc98dd
Warren Watson <wwatson4506@gmail.com>
2024-04-22 10:49:47 -0700

    Update for T41 8080 mode

--- examples/UserDefinedFonts/UserDefinedFonts.ino
+++ examples/UserDefinedFonts/UserDefinedFonts.ino
@@ -2,12 +2,33 @@
 #include "Arduino.h"
 #include "RA8876_t3.h"
 
-#include "font8x16.h"
+//*************************************** 8 BIT BUS ONLY RIGHT NOW. DOES NOT WORK WITH 16 BIT BUS. *********************************
+//#define RA8876_CS 10
+//#define RA8876_RESET 8
+#define BACKLITE 5 // was 7 //External backlight control connected to this Arduino pin
+//RA8876_t3 tft = RA8876_t3(RA8876_CS, RA8876_RESET); //Using standard SPI pins
 
-#define RA8876_CS 10
-#define RA8876_RESET 9
-#define BACKLITE 7 //External backlight control connected to this Arduino pin
-RA8876_t3 tft = RA8876_t3(RA8876_CS, RA8876_RESET); //Using standard SPI pins
+/*
+// MicroMod
+uint8_t dc = 13;
+uint8_t cs = 11;
+uint8_t rst = 5;
+*/
+/*
+// SDRAM DEV board V4.0
+uint8_t dc = 17;
+uint8_t cs = 14;
+uint8_t rst = 27;
+*/
+
+// T4.1
+uint8_t dc = 13;
+uint8_t cs = 11;
+uint8_t rst = 12;
+
+RA8876_t3 tft = RA8876_t3(dc,cs,rst); //(dc, cs, rst)
+
+#include "font8x16.h"
 
 // Array of Simple RA8876 Basic Colors
 PROGMEM uint16_t myColors[] = {
@@ -40,30 +61,32 @@ void setup() {
   //backlight control instead of the internal RA8876 PWM.
   //Connect a Teensy pin to pin 14 on the display.
   //Can use analogWrite() but I suggest you increase the PWM frequency first so it doesn't sing.
-  pinMode(BACKLITE, OUTPUT);
-  digitalWrite(BACKLITE, HIGH);
-  tft.begin();
-  //initVT100();
-  tft.fontLoadMEM((char *)font8x16);
-  tft.setFontSize(1,false);
-  tft.setCursor(0,0);
+//  pinMode(BACKLITE, OUTPUT);
+//  digitalWrite(BACKLITE, HIGH);
+      
+//	tft.begin();
+	tft.begin(20);
+	//initVT100();
+	tft.fontLoadMEM((char *)font8x16);
+	tft.setFontSize(1,false);
+	tft.setCursor(0,0);
   tft.println("Font Test");
-  tft.fillScreen(myColors[0]);
-  tft.fillStatusLine(myColors[0]);
-  tft.printStatusLine(0,myColors[13],myColors[0],"Example of User Defined Characters. 8x16 font size. Uses tftRawWrite() function.");
-  tft.setFontSource(1); //Select UDFont.
-  tft.setTextColor(myColors[1] , myColors[0]);
-  tft.setFontSize(0,true); // Set to smallest font scale.
-  tft.printf("Hello Teensy!, font scaling = 0\n"); 
-  tft.setFontSize(1,true); // Set to 1X font scale.
-  tft.printf("Hello Teensy!, font scaling = 1\n"); 
-  tft.setFontSize(2,true); // Set to 2X font scale.
-  tft.printf("Hello Teensy!, font scaling = 2\n\n"); 
-  tft.setFontSize(1,true); // Set to smallest font scale.
-  tft.setTextColor(myColors[6] , myColors[0]);
-  // Send raw characters to screen. Does not process ASCII control codes.
-  for(uint8_t i = 0; i < 255; i++)
-    tft.rawPrint(i);
+	tft.fillScreen(myColors[0]);
+	tft.fillStatusLine(myColors[0]);
+	tft.printStatusLine(0,myColors[13],myColors[0],"Example of User Defined Characters. 8x16 font size. Uses tftRawWrite() function.");
+	tft.setFontSource(1); //Select UDFont.
+	tft.setTextColor(myColors[1] , myColors[0]);
+	tft.setFontSize(0,true); // Set to smallest font scale.
+	tft.printf("Hello Teensy!, font scaling = 0\n"); 
+	tft.setFontSize(1,true); // Set to 1X font scale.
+	tft.printf("Hello Teensy!, font scaling = 1\n"); 
+	tft.setFontSize(2,true); // Set to 2X font scale.
+	tft.printf("Hello Teensy!, font scaling = 2\n\n"); 
+	tft.setFontSize(1,true); // Set to smallest font scale.
+	tft.setTextColor(myColors[6] , myColors[0]);
+	// Send raw characters to screen. Does not process ASCII control codes.
+	for(uint8_t i = 0; i < 255; i++)
+		tft.rawPrint(i);
 }
 
 void loop() {
