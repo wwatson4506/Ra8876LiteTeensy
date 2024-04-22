fad4130288ff3cff5abbf2fc707175417c91940f
Warren Watson <wwatson4506@gmail.com>
2024-04-22 10:46:00 -0700

    Update for T41 8080 mode

--- examples/ILI_Ada_FontTest4/ILI_Ada_FontTest4.ino
+++ examples/ILI_Ada_FontTest4/ILI_Ada_FontTest4.ino
@@ -1,23 +1,44 @@
-#include <Adafruit_GFX.h>
-
-#include <SPI.h>
 #include "RA8876_t3.h"
-#define RA8876_CS 10
-#define RA8876_RESET 9
-#define BACKLITE 7 //External backlight control connected to this Arduino pin
+
+/*
+// MicroMod
+uint8_t dc = 13;
+uint8_t cs = 11;
+uint8_t rst = 5;
+*/
+/*
+// SDRAM DEV board V4.0
+uint8_t dc = 17;
+uint8_t cs = 14;
+uint8_t rst = 27;
+*/
+
+// T4.1
+uint8_t dc = 13;
+uint8_t cs = 11;
+uint8_t rst = 12;
+
+RA8876_t3 tft = RA8876_t3(dc,cs,rst); //(dc, cs, rst)
+
+//#define RA8876_CS 10
+//#define RA8876_RESET 8
+#define BACKLITE 5 // was 7 //External backlight control connected to this Arduino pin
+//RA8876_t3 tft = RA8876_t3(RA8876_CS, RA8876_RESET); //Using standard SPI pins
+
+#include "_fonts.h"
 
 #include "font_Arial.h"
 #include "font_ArialBold.h"
-#include "font_ComicSansMS.h"
-#include "font_OpenSans.h"
-#include "font_DroidSans.h"
-#include "font_Michroma.h"
-#include "font_Crystal.h"
-#include "font_ChanceryItalic.h"
+#include "_font_ComicSansMS.h"
+#include "_font_OpenSans.h"
+//#include "font_DroidSans.h"
+//#include "font_Michroma.h"
+//#include "font_Crystal.h"
+//#include "font_ChanceryItalic.h"
 
 // maybe a few GFX FOnts?
-#include <Fonts/FreeMonoBoldOblique12pt7b.h>
-#include <Fonts/FreeSerif12pt7b.h>
+//#include <Fonts/FreeMonoBoldOblique12pt7b.h>
+//#include <Fonts/FreeSerif12pt7b.h>
 
 typedef struct {
   const ILI9341_t3_font_t *ili_font;
@@ -35,35 +56,31 @@ const ili_fonts_test_t font_test_list[] = {
   {&Arial_14, nullptr,  "Arial_14", WHITE, WHITE},
   {&Arial_14_Bold, nullptr,  "ArialBold 14", YELLOW, YELLOW},
   {&ComicSansMS_14, nullptr,  "ComicSansMS 14", GREEN, GREEN},
-  {&DroidSans_14, nullptr,  "DroidSans_14", WHITE, WHITE},
-  {&Michroma_14, nullptr,  "Michroma_14", YELLOW, YELLOW},
-  {&Crystal_24_Italic, nullptr,  "CRYSTAL_24", BLACK, YELLOW},
-  {&Chancery_24_Italic, nullptr,  "Chancery_24_Italic", GREEN, GREEN},
+//  {&DroidSans_14, nullptr,  "DroidSans_14", WHITE, WHITE},
+//  {&Michroma_14, nullptr,  "Michroma_14", YELLOW, YELLOW},
+//  {&Crystal_24_Italic, nullptr,  "CRYSTAL_24", BLACK, YELLOW},
+//  {&Chancery_24_Italic, nullptr,  "Chancery_24_Italic", GREEN, GREEN},
   {&OpenSans24, nullptr,  "OpenSans 18", RED, YELLOW},
-  {nullptr, &FreeMonoBoldOblique12pt7b,  "GFX FreeMonoBoldOblique12pt7b", WHITE, WHITE},
-  {nullptr, &FreeMonoBoldOblique12pt7b,  "GFX FreeMonoBoldOblique12pt7b", RED, YELLOW},
-  {nullptr, &FreeSerif12pt7b,  "GFX FreeSerif12pt7b", WHITE, WHITE},
-  {nullptr, &FreeSerif12pt7b,  "GFX FreeSerif12pt7b", RED, YELLOW},
+//  {nullptr, &FreeMonoBoldOblique12pt7b,  "GFX FreeMonoBoldOblique12pt7b", WHITE, WHITE},
+//  {nullptr, &FreeMonoBoldOblique12pt7b,  "GFX FreeMonoBoldOblique12pt7b", RED, YELLOW},
+//  {nullptr, &FreeSerif12pt7b,  "GFX FreeSerif12pt7b", WHITE, WHITE},
+//  {nullptr, &FreeSerif12pt7b,  "GFX FreeSerif12pt7b", RED, YELLOW},
 
 } ;
 
 extern void displayStuff(const char *font_name);
-
-
-RA8876_t3 tft = RA8876_t3(RA8876_CS, RA8876_RESET); //Using standard SPI pins
-
 uint8_t test_screen_rotation = 0;
-
 
 void setup() {
   Serial.begin(38400);
   long unsigned debug_start = millis ();
   while (!Serial && ((millis () - debug_start) <= 5000)) ;
   Serial.println("Setup");
-  tft.begin(30000000);
+
+  tft.begin(20);
   tft.backlight(true);
-  pinMode(BACKLITE, OUTPUT);
-  digitalWrite(BACKLITE, HIGH);
+//  pinMode(BACKLITE, OUTPUT);
+//  digitalWrite(BACKLITE, HIGH);
 
 //  tft.setRotation(4);
   tft.fillScreen(BLACK);
@@ -78,7 +95,7 @@ void setup() {
   tft.setTextColor(GREEN);
   tft.setFont(ComicSansMS_14);
   displayStuff("ComicSansMS 14");
-
+/*
   nextPage();
   tft.setTextColor(WHITE);
   tft.setFont(DroidSans_14);
@@ -92,13 +109,13 @@ void setup() {
   tft.setTextColor(BLACK, YELLOW);
   tft.setFont(Crystal_24_Italic);
   displayStuff("CRYSTAL_24");
-
+*/
   nextPage();
-
+/*
   tft.setTextColor(GREEN);
   tft.setFont(Chancery_24_Italic);
   displayStuff("Chancery_24_Italic");
-
+*/
   //anti-alias font OpenSans
   tft.setTextColor(RED, YELLOW);
   tft.setFont(OpenSans24);
